f40d90e15d0320e3f158045e3fb4d5f4d9a5a2db
Sunkar <sunkardimon@gmail.com>
2025-03-30 18:32:18 +0500

    final version

--- S-Company/src/pages/Home.jsx
+++ S-Company/src/pages/Home.jsx
@@ -1,10 +1,20 @@
 import React from "react";
-import MyCarousel from "../components/Carousel"; // Импорт слайдера
+import MyCarousel from "../components/Carousel";
+import "../CSS/Home.css"; // Подключаем стили
 
 const Home = () => {
   return (
-    <div>
+    <div className="home-container">
       <MyCarousel />
+      <section className="merchandising-info">
+        <h2>Почему важен мерчандайзинг?</h2>
+        <p>
+          Мерчандайзинг — это не просто расстановка товаров на полках, а целая
+          наука, помогающая увеличивать продажи и привлекать покупателей. 
+          Грамотная выкладка, контроль запасов и анализ спроса обеспечивают 
+          эффективную работу магазинов и высокую лояльность клиентов.
+        </p>
+      </section>
     </div>
   );
 };
